a35ef16cca055270334f23265748d33231d7d952
Jai Masters <126451225+jaimasters@users.noreply.github.com>
2023-12-16 13:07:11 +1100

    improved visulisation
    use arduino ide graphing tool
    - displays all derivitives of displacement to 2nd order
    - displays pid output magnitude (thrust %)

--- EncoderTest_2/EncoderTest_2.ino
+++ EncoderTest_2/EncoderTest_2.ino
@@ -5,11 +5,18 @@
 #define ENCODER_B 33  // Pin for Encoder B
 #define PI 3.14159265
 
+// PID
+#include <PID_v2.h>
+double Kp = 20, Ki = 50, Kd = 10; //order of magnitude 10-1000
+PID_v2 myPID(Kp, Ki, Kd, PID::Direct);
+
 float goal_velocity = 0.9;
 float err = 0.0;
 float vel = 0.0;
 float old_vel = 0.0;
 float accel = 0.0;
+float PID_out_magnitude = 0.0;
+int PID_out_pwm = 0.0;
 
 volatile int elapsed_ticks = 0;
 volatile int displacement_ticks = 0; // Global variable for storing the encoder position
@@ -44,7 +51,14 @@ float err_amount(float velocity, float goal){
   return((velocity-goal)/goal);
 }
 
+void PID() {
+  float PID_raw = myPID.Run(vel);
+  if (PID_raw > 100) {PID_raw = 100;}
+  PID_out_pwm = (PID_raw*5) + 1500;
+  PID_out_magnitude = PID_raw / 100.0f;
 
+
+}
 
 void setup() {
   Serial.begin(115200); // Initialize serial communication
@@ -53,6 +67,10 @@ void setup() {
   pinMode(ENCODER_B, INPUT_PULLUP);
   // Attaching the ISR to encoder A
   attachInterrupt(digitalPinToInterrupt(ENCODER_A), encoder_isr, CHANGE);
+
+  myPID.Start(vel,                // input
+              0,                  // current output
+              goal_velocity);     // setpoint
 }
 
 
@@ -62,12 +80,16 @@ delayMicroseconds(time_interval);
 old_vel = vel;
 vel = measure_velocity();
 accel = measure_accel(vel, old_vel);
-
+PID();
 
 // use this print format so you can disable individual plots on the plotter window
-Serial.print("velocity:"+ String(vel,10)+",");
-Serial.print("goal:"+ String(goal_velocity,10)+",");
-Serial.print("accel:"+ String(accel,10)+",");
+Serial.print("zeroline...Div's_displacement:"+ String(0,10)+",");
+Serial.print("cur:"+ String(displacement_ticks,10)+",");
+Serial.print("cur':"+ String(vel,10)+",");
+Serial.print("tar':"+ String(goal_velocity,10)+",");
+Serial.print("cur'':"+ String(accel,10)+",");
+Serial.print("PID:"+ String(PID_out_magnitude,10)+",");
+Serial.print("PWM:"+ String(PID_out_pwm,10)+",");
 
 //Serial.print("error:"+ String(err_amount(vel,goal_velocity),10)+",");
 //Serial.print("zero:"+ String(0,10)+",");
