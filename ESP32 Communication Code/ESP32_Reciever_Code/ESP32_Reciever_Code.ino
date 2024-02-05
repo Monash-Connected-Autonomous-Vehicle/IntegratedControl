51784755910b4bc6987c8f36eedebfb375322386
Credit3 <142369108+Credit3@users.noreply.github.com>
2024-02-05 17:45:11 +1100

    The green is good until

---
+++ ESP32 Communication Code/ESP32_Reciever_Code/ESP32_Reciever_Code.ino
@@ -0,0 +1,93 @@
+
+
+// SimpleRx - the slave or the receiver
+#include <SPI.h>
+#include <RF24.h>
+#include <nRF24L01.h>
+
+#define SERVO1 1
+#define SERVO2 2
+#define LED_BUTTON 14
+#define LED_SWITCH 12
+
+#define CE_PIN  22
+#define CSN_PIN 21
+const byte thisSlaveAddress[5] = {'R', 'x', 'A', 'A', 'A'};
+RF24 radio(CE_PIN, CSN_PIN);
+char dataReceived[10]; // this must match dataToSend in the TX
+int xValData;
+int yValData;
+bool newData = false;
+
+struct data_pack{
+  int xValuePack;
+  int yValuePack;
+  int buttonState;
+  int togSwitchVal;
+};
+
+data_pack data_var;
+
+//===========
+void setup()
+{
+   Serial.begin(115200);
+   delay(3000);
+   Serial.println("SimpleRx Starting");
+   radio.begin();
+   radio.setDataRate( RF24_250KBPS );
+   radio.openReadingPipe(1, thisSlaveAddress);
+   radio.startListening();
+   pinMode(LED_BUTTON, OUTPUT);
+   pinMode(LED_SWITCH, OUTPUT);
+}
+//=============
+void loop()
+{
+   getData();
+   showData();
+   ledLightUp();
+}
+//==============
+void getData()
+{
+   if ( radio.available() )
+   {
+      radio.read( &data_var, sizeof(data_var) );
+      
+      newData = true;
+   }
+}
+
+int getControllerPosData(){
+  
+}
+
+void showData()
+{
+   if (newData == true)
+   {
+      Serial.print("Data received: x-value =  ");
+      Serial.print(data_var.xValuePack);
+      Serial.print(", y-value = ");
+      Serial.print(data_var.yValuePack);
+      Serial.print(", Button-value = ");
+      Serial.print(data_var.buttonState);
+      Serial.print(", Switch-value = ");
+      Serial.println(data_var.togSwitchVal);
+      newData = false;
+   }
+}
+
+void ledLightUp(){
+  if(!data_var.buttonState){
+    digitalWrite(LED_BUTTON, HIGH);
+   } else{
+    digitalWrite(LED_BUTTON, LOW);
+   }
+  if(!data_var.togSwitchVal){
+    digitalWrite(LED_SWITCH, HIGH);
+  } else{
+    digitalWrite(LED_SWITCH, LOW);
+  }
+}
---
+++ ESP32 Communication Code/ESP32_Sender_Code/ESP32_Sender_Code.ino
@@ -0,0 +1,105 @@
+// sender code
+// SimpleTx - the master or the transmitter
+
+// ADD A JOYSTICK CODE
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+#define CE_PIN 22
+#define CSN_PIN 21
+
+#define VRX_PIN_LEFT_JOYSTICK 35   // Controls Left Joystick. X position (Horizontal)
+#define VRY_PIN_RIGHT_JOYSTICK 34  // Controls Right Joystick. Y position (Vertical)
+#define button 12
+#define toggle_switch 14
+
+int xValue = 0;  // Initial position at (0,0)
+int yValue = 0;  // Initial position at (0,0)
+
+// int prevState = HIGH;
+// int currentState;
+
+const byte thisSlaveAddress[5] = { 'R', 'x', 'A', 'A', 'A' };
+RF24 radio(CE_PIN, CSN_PIN);
+char dataToSend[10] = "Message 0";
+char txNum = '0';
+unsigned long currentMillis;
+unsigned long prevMillis;
+unsigned long txIntervalMillis = 200;  // send once per second
+
+struct data_pack {
+  int xValuePack;
+  int yValuePack;
+  int buttonState;
+  int togSwitchVal;
+};
+
+data_pack data_var;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(VRX_PIN_LEFT_JOYSTICK, INPUT);
+  pinMode(VRY_PIN_RIGHT_JOYSTICK, INPUT);
+  Serial.println("SimpleTx Starting");
+  radio.begin();
+  radio.setDataRate(RF24_250KBPS);
+  radio.setRetries(3, 5);  // delay, count
+  radio.openWritingPipe(thisSlaveAddress);
+  pinMode(button, INPUT_PULLUP);
+  pinMode(toggle_switch, INPUT_PULLUP);
+}
+
+void loop() {
+
+  data_var.xValuePack = analogRead(VRX_PIN_LEFT_JOYSTICK);
+  data_var.yValuePack = analogRead(VRY_PIN_RIGHT_JOYSTICK);
+  data_var.buttonState = digitalRead(button);
+  data_var.togSwitchVal = digitalRead(toggle_switch);
+  currentMillis = millis();
+  if (currentMillis - prevMillis >= txIntervalMillis) {
+    // Serial.print("x = ");
+    // Serial.print(data_var.xValuePack);
+    // Serial.print(", y = ");
+    // Serial.println(data_var.yValuePack);
+
+    send(data_var.xValuePack, data_var.yValuePack, data_var.buttonState, data_var.togSwitchVal);
+    prevMillis = millis();
+  }
+}
+
+void send(int X_VAL_HORIZONTAL, int Y_VAL_VERTICAL, int BUTTON_VAL, int SWITCH_VAL) {
+  bool rslt_data_bool, rslt_button_bool;
+
+  //  rslt_message = radio.write(&dataToSend, sizeof(dataToSend));
+  rslt_data_bool = radio.write(&data_var, sizeof(data_var));
+
+
+  // Always use sizeof() as it gives the size as the number of bytes.
+  // For example if dataToSend was an int sizeof() would correctly return 2
+  Serial.print("Data Sent ");
+  Serial.print(dataToSend);
+  if (rslt_data_bool) {
+    Serial.print(" Acknowledge received:");
+    Serial.print("x-value = ");
+    Serial.print(X_VAL_HORIZONTAL);
+    Serial.print(", y-value = ");
+    Serial.print(Y_VAL_VERTICAL);
+    Serial.print(", Button-value = ");
+    Serial.print(BUTTON_VAL);
+    Serial.print(", Switch-value = ");
+    Serial.println(SWITCH_VAL);
+    updateMessage();
+  } else {
+    Serial.println(" Tx failed");
+  }
+}
+void updateMessage() {
+  // so you can see that new data is being sent
+  txNum += 1;
+  if (txNum > '9') {
+    txNum = '0';
+  }
+  dataToSend[8] = txNum;
+}
