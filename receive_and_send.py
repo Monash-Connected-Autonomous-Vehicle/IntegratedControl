cd5867cdd678531e67375139bbb10c8d2849d7ad
Sam Tri <142369108+Credit3@users.noreply.github.com>
2024-04-02 18:01:14 +1100

    Add files via upload

---
+++ receive_and_send.py
@@ -0,0 +1,85 @@
+import struct
+import serial
+import serial.tools.list_ports
+import time
+import math
+
+def get_data_from_CAN_bridge():
+    byteArrayIncoming = bytearray()
+    dataIncoming = None
+    IDDummy = None
+    cntr = 0
+		
+    if IDDummy == None:
+        for i in range(1):	
+            IDDummy = serial_connection.read()
+        while dataIncoming == None:
+            cntr+=1
+            dataIncoming = serial_connection.read()
+            if cntr == 1:
+                ID = dataIncoming
+            byteArrayIncoming.extend(dataIncoming) 
+        
+        for i in range(4):
+            dataIncoming = serial_connection.read()
+            if i != 0:
+                byteArrayIncoming.extend(dataIncoming)
+            else:
+                continue
+        
+        # print(Bytesarray)
+        data = struct.unpack('f', byteArrayIncoming)
+        cntr = 0
+        ID = ID[0]
+        
+        data = data[0]
+        print(f"Received ID from CAN = {ID}")
+        print(f"Received Data from CAN = {data}")
+        print()
+    
+
+def pass_data_to_CAN_Bridge():
+    data = 69
+
+
+    packet = bytearray()
+    packet.append(69)
+
+    val = 42069.214
+    while True:
+        
+
+        print(packet)
+
+
+
+
+    try:
+        ##serial_connection.write(binary_out)
+        ##serial_connection.write(binary_data.encode('utf-8'))
+        sleep(1000)
+        serial_connection.write(packet)
+        print(data)
+        
+    except serial.SerialException as e:
+        print("bruh: " + e)
+
+    pass
+    
+
+
+serial_port = '/dev/ttyUSB1' # change back to 0 when USB0 works again
+baud_rate = 115200
+serial_connection = serial.Serial(serial_port, baud_rate, timeout=1, bytesize=8)
+
+cntr = 0
+while True:
+    try:
+        get_data_from_CAN_bridge()
+
+        # pass_data_to_CAN_Bridge()
+
+        
+
+    except serial.SerialException as e:
+        print("BRUH: " + e)
