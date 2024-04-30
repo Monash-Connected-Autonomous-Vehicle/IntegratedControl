02f52841727d1a8388597409e38812e45580d9ab
BMCG0011 <bmcg0011@student.monash.edu>
2024-04-30 13:15:48 +1000

    Before implement second motor/enc

--- CAN_ESC_Controller/CAN_ESC_Controller.ino
+++ CAN_ESC_Controller/CAN_ESC_Controller.ino
@@ -1,5 +1,3 @@
-#include <ESP32_PWM.h>
-#include <ESP32_PWM.hpp>
 
 /* 
 |--------------< CONTENTS >---------------|
@@ -14,10 +12,16 @@
 */
 
 // ENUM DEFINITIONS
-enum CANPacketType {
-  Displacement = 1,
-  Velocity = 2,
-  TurnAmount = 3,
+enum ESDACanMessage {
+    SetTargetVelLeft = 1,
+    SetTargetVelRight = 2,
+    CurrentVelLeft = 3,
+    CurrentVelRight = 4,
+    CurrentDSPLeft = 5,
+    CurrentDSPRight = 6,
+    MCUState = 16,
+    MCUErrorState = 17,
+    ESTOP = 8
 };
 
 // GLOBALS
@@ -27,11 +31,24 @@ const uint32_t TIME_INTERVAL = 100000;   //Unit: Microseconds
 const uint32_t TICKS_PER_ROTATION = 1024;  //how many encoder ticks per roation of wheel
 const float WHEEL_RADIUS = 0.13;      //Unit: Metres
 
-// Pin Definitions
-const uint32_t PIN_ESC_A_PWM = 14; // PWM Output Pin for ESC A
-const uint32_t PIN_ESC_B_PWM = 15; // PWM Output Pin for ESC B
-const uint32_t PIN_RENC_A1 = 32;  // Pin for Rotary Encoder A
-const uint32_t PIN_RENC_A2 = 33;  // Pin for Rotary Encoder B
+// // Pin Definitions
+const uint32_t PIN_ESC_A_PWM = 26; // PWM Output Pin for ESC A
+//const uint32_t PIN_ESC_B_PWM = 15; // PWM Output Pin for ESC B
+const uint32_t PIN_RENC_A1 = 17;  // Pin for Rotary Encoder A
+const uint32_t PIN_RENC_A2 = 33;  // Pin for Rotary Encoder B 34?
+//const uint32_t PIN_RENC_B1 = ;  // Pin for Rotary Encoder A
+//const uint32_t PIN_RENC_B2 = ;  // Pin for Rotary Encoder B
+
+// #define HW_TIMER_INTERVAL_US 20L
+// #define TIME_INTERVAL 100000   //Unit: Microseconds
+// #define TICKS_PER_ROTATION 1024  //how many encoder ticks per roation of wheel
+// #define WHEEL_RADIUS 0.13     //Unit: Metres
+
+// // Pin Definitions
+// #define PIN_ESC_A_PWM 5 // PWM Output Pin for ESC A
+// #define PIN_ESC_B_PWM 18 // PWM Output Pin for ESC B
+// #define PIN_RENC_A1   19 // Pin for Rotary Encoder A
+// #define PIN_RENC_A2   21 // Pin for Rotary Encoder B
 //const uint32_t PIN_RENC_B1 = ;  // Pin for Rotary Encoder A
 //const uint32_t PIN_RENC_B2 = ;  // Pin for Rotary Encoder B
 
@@ -65,9 +82,9 @@ ESP32_PWM ISR_PWM;
 
 bool IRAM_ATTR TimerHandler(void * timerNo)
 {
-  ISR_PWM.run();
+    ISR_PWM.run();
 
-  return true;
+    return true;
 }
 
 #define USING_PWM_FREQUENCY     false
@@ -77,16 +94,16 @@ int channelNum;
 
 float throttleToDuty(float throttle)
 {
-  // Assume the vehicles ESC which maps 7.5% to neutural, 5% reverse, 10% forward (full)
-  // function: duty(throttle) = throttle * multipier + constant
-  //float mult = 0.025;
-  //float cons = 7.5;
+    // Assume the vehicles ESC which maps 7.5% to neutural, 5% reverse, 10% forward (full)
+    // function: duty(throttle) = throttle * multipier + constant
+    //float mult = 0.025;
+    //float cons = 7.5;
 
-  float mult = 0.01;
-  float cons = 7.5;
+    float mult = 0.01;
+    float cons = 7.5;
 
-  // adding 25 because of deadzoneX
-  return (throttle + 25) * mult + cons;
+    // adding 25 because of deadzoneX
+    return (throttle + 25) * mult + cons;
 }
 
 // Frequency of PWM used to control the ESC
@@ -94,11 +111,11 @@ const float PWM_FREQ = 50;
 
 void setPWMThrottle(int pin, int channel, float throttle)
 {
-  // You can use this with PWM_Freq in Hz
-  if (!ISR_PWM.modifyPWMChannel(channel, pin, PWM_FREQ, throttleToDuty(throttle)))
-  {
-    Serial.print(F("modifyPWMChannel error for PWM_Period"));
-  }
+    // You can use this with PWM_Freq in Hz
+    if (!ISR_PWM.modifyPWMChannel(channel, pin, PWM_FREQ, throttleToDuty(throttle)))
+    {
+        Serial.print(F("modifyPWMChannel error for PWM_Period"));
+    }
 }
 
 ///////////////////////////////
@@ -125,42 +142,42 @@ volatile int DRAM_ATTR pidDisplacementTicks = 0; // Global variable for storing 
 
 // Interrupt callback function for rotary encoders
 void encoder_isr() {
-  // Reading the current state of encoder A and B
-  int A = digitalRead(PIN_RENC_A1);
-  int B = digitalRead(PIN_RENC_A2);
-  // If the state of A changed, it means the encoder has been rotated
-  if ((A == HIGH) != (B == LOW)) {
-    pidDisplacementTicks--;
-    pidElapsedTicks--;
-  } else {
-    pidDisplacementTicks++;
-    pidElapsedTicks++;
-  }
+    // Reading the current state of encoder A and B
+    int A = digitalRead(PIN_RENC_A1);
+    int B = digitalRead(PIN_RENC_A2);
+    // If the state of A changed, it means the encoder has been rotated
+    if ((A == HIGH) != (B == LOW)) {
+        pidDisplacementTicks--;
+        pidElapsedTicks--;
+    } else {
+        pidDisplacementTicks++;
+        pidElapsedTicks++;
+    }
 }
 
 // Math function for ang disp -> lin vel
 float measureVelocity() {
-  float rotationsPerInterval = float(pidElapsedTicks) / float(TICKS_PER_ROTATION); 
-  float metersPerInterval = rotationsPerInterval * float(WHEEL_RADIUS) * 2 * PI;
-  float timeIntervalFloatified = float(TIME_INTERVAL)/float(1000000); 
-  pidElapsedTicks = 0; //reset accumilator 
-  return (metersPerInterval / timeIntervalFloatified);
+    float rotationsPerInterval = float(pidElapsedTicks) / float(TICKS_PER_ROTATION); 
+    float metersPerInterval = rotationsPerInterval * float(WHEEL_RADIUS) * 2 * PI;
+    float timeIntervalFloatified = float(TIME_INTERVAL)/float(1000000); 
+    pidElapsedTicks = 0; //reset accumilator 
+    return (metersPerInterval / timeIntervalFloatified);
 }
 
 float measureAcceleration(float velocity, float velocityOld){
-  return((velocity - velocityOld)/(float(TIME_INTERVAL)/float(1000000)));
+    return((velocity - velocityOld)/(float(TIME_INTERVAL)/float(1000000)));
 }
 
 float errorAmount(float velocity, float velocityGoal){
-  return((velocity-velocityGoal)/velocityGoal);
+    return((velocity-velocityGoal)/velocityGoal);
 }
 
 float PID() {
-  float pidRAW = myPID.Run(velocity);
-  if (pidRAW > 100) {pidRAW = 100;}
-  pidOutputPWM = (pidRAW*5) + 1500;
-  pidOutputMagnitude = pidRAW / 100.0f;
-  return pidRAW;
+    float pidRAW = myPID.Run(velocity);
+    if (pidRAW > 100) {pidRAW = 100;}
+    pidOutputPWM = (pidRAW*5) + 1500;
+    pidOutputMagnitude = pidRAW / 100.0f;
+    return pidRAW;
 }
 
 //////////
@@ -169,27 +186,27 @@ float PID() {
 #include <CAN.h>
 
 // Pins for CAN Transciever
-const uint16_t PIN_CAN_TX = 21;
-const uint16_t PIN_CAN_RX = 22;
+const uint16_t PIN_CAN_TX = 23;
+const uint16_t PIN_CAN_RX = 25;
 
 // Link Speed for CANBUS in bps
 const long CAN_BAUDRATE = 500E3;
 
 // Function to setup canbus
 void setupCAN() {
-  Serial.println("Initializing CAN Transciever...");
-  // Set CAN Pin States
-  CAN.setPins(PIN_CAN_RX,PIN_CAN_TX);
-  // Start the CAN Bus and wait for it to come online
-  if (!CAN.begin(CAN_BAUDRATE)) {
-    Serial.println("Failed to start CAN!");
-    // Pause indefinitely
-    while (1);
-  } else {
-    Serial.println("CAN bus Initialized Successfully");
-  }
-  Serial.println("Adding CAN Recieve Hook");
-  CAN.onReceive(onRecieveCAN);
+    Serial.println("Initializing CAN Transciever...");
+    // Set CAN Pin States
+    CAN.setPins(PIN_CAN_RX,PIN_CAN_TX);
+    // Start the CAN Bus and wait for it to come online
+    if (!CAN.begin(CAN_BAUDRATE)) {
+        Serial.println("Failed to start CAN!");
+        // Pause indefinitely
+        while (1);
+    } else {
+        Serial.println("CAN bus Initialized Successfully");
+    }
+    Serial.println("Adding CAN Recieve Hook");
+    CAN.onReceive(onRecieveCAN);
 }
 
 ////////////////
@@ -198,143 +215,143 @@ void setupCAN() {
 
 void setup() {
 
-  // Initialise UART
-  Serial.begin(115200);
-  // Wait for UART to come online
-  while (!Serial);
+    // Initialise UART
+    Serial.begin(115200);
+    // Wait for UART to come online
+    while (!Serial);
 
-  // Sleep for 2 Seconds
-  delay(2000);
- 
-  // ESC AND PWM INIT //
-  Serial.print(F("\nStarting ISR_Modify_PWM on ")); Serial.println(ARDUINO_BOARD);
-  Serial.println(ESP32_PWM_VERSION);
-  Serial.print(F("CPU Frequency = ")); Serial.print(F_CPU / 1000000); Serial.println(F(" MHz"));
+    // Sleep for 2 Seconds
+    delay(2000);
+    
+    // ESC AND PWM INIT //
+    Serial.print(F("\nStarting ISR_Modify_PWM on ")); Serial.println(ARDUINO_BOARD);
+    Serial.println(ESP32_PWM_VERSION);
+    Serial.print(F("CPU Frequency = ")); Serial.print(F_CPU / 1000000); Serial.println(F(" MHz"));
 
-  // Setup timer with interval //
-  if (ITimer.attachInterruptInterval(HW_TIMER_INTERVAL_US, TimerHandler))
-  {
-    startTimeMicroseconds = micros();
-    Serial.print(F("Starting ITimer OK, micros() = ")); Serial.println(startTimeMicroseconds);
-  }
-  else
-    Serial.println(F("Can't set ITimer. Select another freq. or timer"));
-  // TODO: Handle ESC B
-  channelNum = ISR_PWM.setPWM_Period(PIN_ESC_A_PWM, PWM_FREQ, 7.5f); // Begins at 1500us 
-  Serial.println("Arming ESCS");
-  delay(5000); // Allow ESC time to arm
+    // Setup timer with interval //
+    if (ITimer.attachInterruptInterval(HW_TIMER_INTERVAL_US, TimerHandler))
+    {
+        startTimeMicroseconds = micros();
+        Serial.print(F("Starting ITimer OK, micros() = ")); Serial.println(startTimeMicroseconds);
+    }
+    else
+        Serial.println(F("Can't set ITimer. Select another freq. or timer"));
+    // TODO: Handle ESC B
+    channelNum = ISR_PWM.setPWM_Period(PIN_ESC_A_PWM, PWM_FREQ, 7.5f); // Begins at 1500us 
+    Serial.println("Arming ESCS");
+    delay(10000); // Allow ESC time to arm
 
-  /////////////////////
-  // Encoder Section //
-  /////////////////////
+    /////////////////////
+    // Encoder Section //
+    /////////////////////
 
-  // Set Encoder A PinModes
-  pinMode(PIN_RENC_A1, INPUT_PULLUP);
-  pinMode(PIN_RENC_A2, INPUT_PULLUP);
+    // Set Encoder A PinModes
+    pinMode(PIN_RENC_A1, INPUT_PULLUP);
+    pinMode(PIN_RENC_A2, INPUT_PULLUP);
 
-  // Attaching the ISR to encoder A
-  attachInterrupt(digitalPinToInterrupt(PIN_RENC_A1), encoder_isr, CHANGE);
+    // Attaching the ISR to encoder A
+    attachInterrupt(digitalPinToInterrupt(PIN_RENC_A1), encoder_isr, CHANGE);
 
-  myPID.Start(velocity,          // input
-              0,                 // current output
-              targetVelocity);                // TODO: USE CAN VALUE?
+    myPID.Start(velocity,          // input
+                0,                 // current output
+                targetVelocity);                // TODO: USE CAN VALUE?
 
-  // Setup the can bus
-  setupCAN();
+    // Setup the can bus
+    // setupCAN();
 
-}
+    }
 
-void loop() {
+    void loop() {
 
-  // TODO: Convert global sleep to ISR Delay
-  delayMicroseconds(TIME_INTERVAL);
+    // TODO: Convert global sleep to ISR Delay
+    delayMicroseconds(TIME_INTERVAL);
 
-  // Cache old versions of each order term
-  oldVelocity = velocity;
-  velocity = measureVelocity();
-  acceleration = measureAcceleration(velocity, oldVelocity);
+    // Cache old versions of each order term
+    oldVelocity = velocity;
+    velocity = measureVelocity();
+    acceleration = measureAcceleration(velocity, oldVelocity);
 
-  // TODO: Make pid function a callback (remove globals for better flow)
+    // TODO: Make pid function a callback (remove globals for better flow)
 
-  setPWMThrottle(PIN_ESC_A_PWM,channelNum,PID());
+    setPWMThrottle(PIN_ESC_A_PWM,channelNum,PID());
 
-  // use this print format so you can disable individual plots on the plotter window
-  Serial.print("zeroline...Div's_displacement:"+ String(0,10)+",");
-  Serial.print("cur:"+ String(pidDisplacementTicks,10)+",");
-  Serial.print("cur':"+ String(velocity,10)+",");
-  Serial.print("tar':"+ String(10,10)+",");
-  Serial.print("cur'':"+ String(acceleration,10)+",");
-  Serial.print("PID:"+ String(pidOutputMagnitude,10)+",");
-  Serial.print("PWM:"+ String(pidOutputPWM,10)+",");
+    // use this print format so you can disable individual plots on the plotter window
+    //Serial.print("zeroline...Div's_displacement:"+ String(0,10)+",");
+    //Serial.print("cur:"+ String(pidDisplacementTicks,10)+",");
+    //Serial.print("cur':"+ String(velocity,10)+",");
+    //Serial.print("tar':"+ String(10,10)+",");
+    //Serial.print("cur'':"+ String(acceleration,10)+",");
+    //Serial.print("PID:"+ String(pidOutputMagnitude,10)+",");
+    //Serial.print("PWM:"+ String(pidOutputPWM,10)+",");
 
-  // Serial.print("error:"+ String(errorAmount(velocity,targetVelocity),10)+",");
-  // Serial.print("zero:"+ String(0,10)+",");
+    // Serial.print("error:"+ String(errorAmount(velocity,targetVelocity),10)+",");
+    // Serial.print("zero:"+ String(0,10)+",");
 
-  Serial.println();
+    Serial.println();
 }
 
 union FloatByteUnion
 {
-   byte bytes[4];
-   float real;
+    byte bytes[4];
+    float real;
 };
 
 void onRecieveCAN(int packetSize) {
-  // Serial.print("Recieved ");
+    // Serial.print("Recieved ");
 
-  // Handle extended CAN Packet
-  if (CAN.packetExtended()) {
-    Serial.print("Extended ");
-  }
-
-  if (CAN.packetRtr()) {
-    //Serial.print("RTR ");
-    //TODO: HANDLE RTR
-  }
-
-  // Report Packet ID in Hex
-  //Serial.print("packet with id ");
-  //Serial.print(CAN.packetId(), HEX);
-
-  // Print the requested length if the packet is a Remote Transmit Request
-  if (CAN.packetRtr()) {
-    //Serial.print(" and requested length ");
-    //Serial.print(CAN.packetDlc());
-  }
-  // Otherwise it is a normal packet
-  else {
-    // Print its length
-    //Serial.print(" and length ");
-    //Serial.print(packetSize);
-
-    // Initialise a buffer for the packet data
-    // TODO: MAYBE HARDCODE THIS TO 8?
-    // packetBuffer[packetSize];
-
-    // Read the can packet into the buffer
-    //Serial.print(": Reading CAN packet...");
-    
-
-    switch (CAN.packetId())
-    {
-      case CANPacketType::Velocity:
-        /* code */
-        //Serial.print("Got Velocity: ");
-        FloatByteUnion newTargetVelocity;        
-
-        // Read the Velocity from the BUS
-        newTargetVelocity.bytes[0] = CAN.read();
-        newTargetVelocity.bytes[1] = CAN.read();
-        newTargetVelocity.bytes[2] = CAN.read();
-        newTargetVelocity.bytes[3] = CAN.read();
-
-        // Change target velocity
-        targetVelocity = newTargetVelocity.real;
-        break;
-      
-      default:
-        break;
+    // Handle extended CAN Packet
+    if (CAN.packetExtended()) {
+        Serial.print("Extended ");
     }
-    Serial.println();
-  }
+
+    if (CAN.packetRtr()) {
+        //Serial.print("RTR ");
+        //TODO: HANDLE RTR
+    }
+
+    // Report Packet ID in Hex
+    //Serial.print("packet with id ");
+    //Serial.print(CAN.packetId(), HEX);
+
+    // Print the requested length if the packet is a Remote Transmit Request
+    if (CAN.packetRtr()) {
+        //Serial.print(" and requested length ");
+        //Serial.print(CAN.packetDlc());
+    }
+    // Otherwise it is a normal packet
+    else {
+        // Print its length
+        //Serial.print(" and length ");
+        //Serial.print(packetSize);
+
+        // Initialise a buffer for the packet data
+        // TODO: MAYBE HARDCODE THIS TO 8?
+        // packetBuffer[packetSize];
+
+        // Read the can packet into the buffer
+        //Serial.print(": Reading CAN packet...");
+        
+
+        switch (CAN.packetId())
+        {
+            case ESDACanMessage::SetTargetVelLeft:
+            /* code */
+            //Serial.print("Got Velocity: ");
+            FloatByteUnion newTargetVelocity;        
+
+            // Read the Velocity from the BUS
+            newTargetVelocity.bytes[0] = CAN.read();
+            newTargetVelocity.bytes[1] = CAN.read();
+            newTargetVelocity.bytes[2] = CAN.read();
+            newTargetVelocity.bytes[3] = CAN.read();
+
+            // Change target velocity
+            targetVelocity = newTargetVelocity.real;
+            break;
+        
+        default:
+            break;
+        }
+        Serial.println();
+    }
 }
