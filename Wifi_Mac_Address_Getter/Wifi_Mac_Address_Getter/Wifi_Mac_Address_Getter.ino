6caec2a3228fd13bc902dbea245ca0aa75f60ee0
Samuel Tri <samueltri5590@gmail.com>
2024-07-11 13:14:18 +1000

    Wifi (Mac Address)
    Codes for wifi communication via mac address

---
+++ Wifi_Mac_Address_Getter/Wifi_Mac_Address_Getter/Wifi_Mac_Address_Getter.ino
@@ -0,0 +1,34 @@
+/*
+  Rui Santos & Sara Santos - Random Nerd Tutorials
+  Complete project details at https://RandomNerdTutorials.com/get-change-esp32-esp8266-mac-address-arduino/
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.  
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+#ifdef ESP32
+  #include <WiFi.h>
+  #include <esp_wifi.h>
+#else
+  #include <ESP8266WiFi.h>
+#endif
+void setup(){
+  Serial.begin(115200);
+  Serial.print("ESP Board MAC Address: ");
+  #ifdef ESP32
+    WiFi.mode(WIFI_STA);
+    WiFi.STA.begin();
+    uint8_t baseMac[6];
+    esp_err_t ret = esp_wifi_get_mac(WIFI_IF_STA, baseMac);
+    if (ret == ESP_OK) {
+      Serial.printf("%02x:%02x:%02x:%02x:%02x:%02x\n",
+                    baseMac[0], baseMac[1], baseMac[2],
+                    baseMac[3], baseMac[4], baseMac[5]);
+    } else {
+      Serial.println("Failed to read MAC address");
+    }
+  #else
+    Serial.println(WiFi.macAddress());
+  #endif
+}
+ 
+void loop(){
+}
---
+++ Wifi_Receiver_Mac_Address/Wifi_Receiver_Mac_Address/Wifi_Receiver_Mac_Address.ino
@@ -0,0 +1,58 @@
+/*********
+  Rui Santos & Sara Santos - Random Nerd Tutorials
+  Complete project details at https://RandomNerdTutorials.com/esp-now-one-to-many-esp32-esp8266/
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*********/
+#include <esp_now.h>
+#include <WiFi.h>
+
+//Structure example to receive data
+//Must match the sender structure
+typedef struct data_pack {
+  float xValuePack;
+  float yValuePack;
+  float buttonState;
+  float togSwitchVal;
+} data_pack;
+
+data_pack myData;
+
+
+//callback function that will be executed when data is received
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&myData, incomingData, sizeof(myData));
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+  Serial.print("xValuePack: ");
+  Serial.println(myData.xValuePack);
+  Serial.print("yValuePack: ");
+  Serial.println(myData.yValuePack);
+  Serial.print("buttonState: ");
+  Serial.println(myData.buttonState);
+  Serial.print("togSwitchVal: ");
+  Serial.println(myData.togSwitchVal);
+  Serial.println();
+}
+ 
+void setup() {
+  //Initialize Serial Monitor
+  Serial.begin(115200);
+  
+  //Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  //Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info
+  esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
+}
+ 
+void loop() {
+
+}
